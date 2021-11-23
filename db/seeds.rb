fe6abeaa752b85415e588e4f6b4f357152355eef
Huiling Loh <huiling_loh_97@hotmail.com>
2021-11-23 14:14:39 +0800

    added model and migrated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# puts "Generating 10 books"
+
+# 10.times do |i|
+#   puts "A book is created"
+#   Article.create({
+#     title: Faker::Book.title,
+#     content: Faker::Books::Dune.quote
+#   })
+# end
+
+# puts "10 books generated!"
